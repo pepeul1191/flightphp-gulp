9f5095e7466f7de7d67d4bb462d4eeec3b96eab8
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-03 21:17:44 -0500

    Validaciones del registro de usuario antes del AJAX de envío

--- app/views/registro/index.php
+++ app/views/registro/index.php
@@ -16,35 +16,50 @@
 </div>
 
 <script id="paso1-template" type="text/x-handlebars-template">
-  	<form id="registro_paso_1_form">
-   	<div class="form-group">
-		   <label class="control-label" for="txtUsuario">Nombre de Usuario</label><span style="color:#a94442; margin-left: 10px"></span>
-		   <input type="text" class="form-control" id="txtUsuario" placeholder="">
-		</div>
-		<div class="form-group">
-		   <label class="control-label" for="txtCorreo">Correo Electrónico</label><span style="color:#a94442; margin-left: 10px"></span>
-		   <input type="email" class="form-control" id="txtCorreo" placeholder="">
-		</div>
-		<div class="form-group">
-		   <label class="control-label" for="txtCorreoRepetir">Repetir Correo Electrónico</label><span style="color:#a94442; margin-left: 10px"></span>
-		   <input type="email" class="form-control" id="txtCorreoRepetir" placeholder="">
-		</div>
-		<div class="form-group">
-		   <label for="txtConstrasenia">Contraseñaa</label><span style="color:#a94442; margin-left: 10px"></span>
-		   <input type="password" class="form-control" id="txtConstrasenia" placeholder="">
-		</div>
-		<div class="form-group">
-		   <label for="txtContraseniaRepetir">Repetir Contraseña</label>
-		   <input type="password" class="form-control" id="txtContraseniaRepetir" placeholder="">
-		</div>
-		<button type="submit" class="btn btn-default" id="btnGuardarPaso1">Guardar y Pasar al Paso 2</button>
-	</form>
+  	<div class="form-group">
+	   <label class="control-label" for="txtUsuario">Nombre de Usuario</label><span style="color:#a94442; margin-left: 10px"></span>
+	   <input type="text" class="form-control" id="txtUsuario" placeholder="">
+	</div>
+	<div class="form-group">
+	   <label class="control-label" for="txtCorreo">Correo Electrónico</label><span style="color:#a94442; margin-left: 10px"></span>
+	   <input type="email" class="form-control" id="txtCorreo" placeholder="">
+	</div>
+	<div class="form-group">
+	   <label class="control-label" for="txtCorreoRepetir">Repetir Correo Electrónico</label><span style="color:#a94442; margin-left: 10px"></span>
+	   <input type="email" class="form-control" id="txtCorreoRepetir" placeholder="">
+	</div>
+	<div class="form-group">
+	   <label for="txtContrasenia">Contraseña</label>
+	   <input type="password" class="form-control" id="txtContrasenia" placeholder="">
+	</div>
+	<div class="form-group">
+	   <label class="control-label" for="txtContraseniaRepetir">Repetir Contraseña</label><span style="color:#a94442; margin-left: 10px"></span>
+	   <input type="password" class="form-control" id="txtContraseniaRepetir" placeholder="">
+	</div>
+	<button type="submit" class="btn btn-default" id="btnGuardarPaso1">Guardar y Pasar al Paso 2</button>
 </script>
 <script type="text/javascript">
 	var Usuario = Backbone.Model.extend({
 	   initialize: function() {
-         //console.log("UsuarioModel");
+         this.usuario_valido = false;
+         this.usuario_lleno = false;
+         this.correo_valido = false;
+         this.contrasenia_valido = false;
          this.valido = false;
+      }, 
+      validar: function() {
+      	if(this.get("usuario_valido") == true && this.get("correo_valido") == true && this.get("contrasenia_valido") == true && this.get("usuario_lleno") == true){
+      		this.set({valido : true});
+      	}else{
+      		this.set({valido : false});
+      	}
+      },
+      toJSON: function() {
+      	var usuario = new Object();
+			usuario.usuario = $("#txtUsuario").val();
+			usuario.contrasenia = $("#txtContrasenia").val();
+			usuario.correo = $("#txtCorreo").val();
+      	return usuario;
       }
 	});
 
@@ -55,8 +70,12 @@
 		},
 		events: {
 		    "keyup #txtUsuario": "validarUsuarioRepetido", 
+		    "focusout #txtUsuario": "validarUsuarioLleno", 
 		    "keyup #txtCorreo": "validarCorreoRepetido", 
 		    "focusout #txtCorreoRepetir": "validarCorreoIgual", 
+		    "focusout #txtContrasenia": "validarContraseniaIgual", 
+		    "focusout #txtContraseniaRepetir": "validarContraseniaIgual", 
+		    "click #btnGuardarPaso1": "GuardarPaso1"
 		},
 		render: function() {
 			var data = { };
@@ -64,9 +83,11 @@
 			var template = Handlebars.compile(source);
 			var template_compiled = template(data);
 			this.$el.html(template_compiled);
+
 			return this;
 		},
 		validarUsuarioRepetido: function(event) {
+			var usuario_valido_valor;
       	$.ajax({
       		type: "POST",
       		url: BASE_URL + "registro/validar_usuario_repetido",
@@ -76,17 +97,34 @@
       			if(data >= 1){
       				$("#txtUsuario").parent().addClass("has-error");
       				$("#txtUsuario").parent().find("span").html("El nombre de usuario registrado ya se encuentra en uso");
+      				usuario_valido_valor = false;
       			}else{
       				$("#txtUsuario").parent().removeClass("has-error");
       				$("#txtUsuario").parent().find("span").html("");
+      				usuario_valido_valor = true;
       			}
       		},
       		error: function(data){
       			//FALTA MANEJAR EL ERROR DEL AJAX
       		}
       	});
+      	this.model.set({usuario_valido : usuario_valido_valor});
+		},
+		validarUsuarioLleno: function(event) {
+			if(this.model.get("usuario_valido") != false){
+				if($("#txtUsuario").val() == ""){
+					$("#txtUsuario").parent().addClass("has-error");
+		      	$("#txtUsuario").parent().find("span").html("Debe ingresar un usuario");
+		      	this.model.set({usuario_lleno : false});
+				}else{
+					$("#txtUsuario").parent().removeClass("has-error");
+		      	$("#txtUsuario").parent().find("span").html("");
+		      	this.model.set({usuario_lleno : true});
+				}
+			}
 		},
 		validarCorreoRepetido: function(event) {
+			var correo_valido_valor;
       	$.ajax({
       		type: "POST",
       		url: BASE_URL + "registro/validar_correo_repetido",
@@ -96,23 +134,69 @@
       			if(data >= 1){
       				$("#txtCorreo").parent().addClass("has-error");
       				$("#txtCorreo").parent().find("span").html("El correo ya se encuentra asociado a un usuario registrado");
+      				correo_valido_valor = false;
       			}else{
       				$("#txtCorreo").parent().removeClass("has-error");
       				$("#txtCorreo").parent().find("span").html("");
+      				correo_valido_valor = true;
       			}
       		},
       		error: function(data){
       			//FALTA MANEJAR EL ERROR DEL AJAX
       		}
       	});
+      	this.model.set({correo_valido : correo_valido_valor});
 		},
 		validarCorreoIgual: function(event) {
-			if($("#txtCorreo").val() != $("#txtCorreoRepetir").val()){
-				$("#txtCorreoRepetir").parent().addClass("has-error");
-      		$("#txtCorreoRepetir").parent().find("span").html("El correo ingresado no coincide con el primero");
+			if($("#txtCorreoRepetir").val() != ""){
+				if($("#txtCorreo").val() != $("#txtCorreoRepetir").val()){
+					$("#txtCorreoRepetir").parent().addClass("has-error");
+	      		$("#txtCorreoRepetir").parent().find("span").html("El correo ingresado no coincide con el primero");
+	      		this.model.set({correo_valido : false});
+				}else{
+					$("#txtCorreoRepetir").parent().removeClass("has-error");
+	      		$("#txtCorreoRepetir").parent().find("span").html("");
+	      		this.model.set({correo_valido : true});
+				}
+			}
+		}, 
+		validarContraseniaIgual: function(event) {
+			if($("#txtContrasenia").val() != $("#txtContraseniaRepetir").val()){
+				$("#txtContraseniaRepetir").parent().addClass("has-error");
+      		$("#txtContraseniaRepetir").parent().find("span").html("La contraseña ingresada no coincide con la primera");
+      		this.model.set({contrasenia_valido : false});
 			}else{
-				$("#txtCorreoRepetir").parent().removeClass("has-error");
-      		$("#txtCorreoRepetir").parent().find("span").html("");
+				$("#txtContraseniaRepetir").parent().removeClass("has-error");
+      		$("#txtContraseniaRepetir").parent().find("span").html("");
+      		this.model.set({contrasenia_valido : true});
+			}
+		}, 
+		GuardarPaso1: function(event){
+			this.model.validar();
+			if(this.model.get("valido") == true){
+				console.log(this.model.toJSON());
+				/*
+				$.ajax({
+	      		type: "POST",
+	      		url: BASE_URL + "registro/guardar_usuario",
+	      		data: "correo=" + $("#txtCorreo").val(),
+	      		async: false,
+	      		success: function(data){
+	      			if(data >= 1){
+	      				$("#txtCorreo").parent().addClass("has-error");
+	      				$("#txtCorreo").parent().find("span").html("El correo ya se encuentra asociado a un usuario registrado");
+	      				correo_valido_valor = false;
+	      			}else{
+	      				$("#txtCorreo").parent().removeClass("has-error");
+	      				$("#txtCorreo").parent().find("span").html("");
+	      				correo_valido_valor = true;
+	      			}
+	      		},
+	      		error: function(data){
+	      			//FALTA MANEJAR EL ERROR DEL AJAX
+	      		}
+	      	});
+				*/
 			}
 		}
 	});
@@ -122,5 +206,4 @@
 		var paso1 = new Paso1View({model:usuario});
 		paso1.render();
 	});
-
 </script>
